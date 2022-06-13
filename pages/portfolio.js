0e9ccd2a6f8c7ae1128a3ac5ec213f8151b97ead
Maicon Ribeiro Esteves <maicon.burn@gmail.com>
2022-06-13 15:07:48 +0100

    Test new jobs api

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,40 +1,62 @@
-import React from 'react'
+import React from "react";
 //import Layout from '../components/Layout'
-import Grid from '@material-ui/core/Grid'
-import JobCard from '../components/JobCard'
-import Query from "../components/api/Query"  
-import JOBS_QUERY from '../apollo/queries/job/jobs'
-import css from '../src/css/pages/portfolio.module.scss'
-import dynamic from 'next/dynamic'
+import Grid from "@material-ui/core/Grid";
+import JobCard from "../components/JobCard";
+import Query from "../components/api/Query";
+import JOBS_QUERY from "../apollo/queries/job/jobs";
+import css from "../src/css/pages/portfolio.module.scss";
+import dynamic from "next/dynamic";
 //eslint-disable-next-line
-const Layout = dynamic(() => import('../components/Layout'), {ssr:false})
+const Layout = dynamic(() => import("../components/Layout"), { ssr: false });
+
+export async function getStaticProps() {
+  const res = await fetch("https://api-portfolio-psi.vercel.app/api/jobs");
+  const posts = await res.json();
+
+  return {
+    props: {
+      jobs,
+    },
+  };
+}
+
+console.log(jobs);
 
 function Portfolio() {
-    const styles = css
-    return (
-        <Layout pageTitle="Projects & developments" pageSubTitle="Showcase of my latest works, projects and developments." className={styles.root}>   
-            <Grid container justify="space-between" alignItems="center" direction="row">
-                <Grid xs={12} align="center" container>
-                    <Query query={JOBS_QUERY} id={null}>
-                        {({ data: { jobs } }) => {
-                            return(
-                                <>
-                                    {jobs.map((job) => {
-                                        return (
-                                            // eslint-disable-next-line react/jsx-key
-                                            <Grid lg={4} md={6} xs={12} item>
-                                                <JobCard key={job.id} job={job} />
-                                            </Grid>
-                                        )
-                                    })} 
-                                </>
-                            ) 
-                        }} 
-                    </Query>
-                </Grid>        
-            </Grid>
-        </Layout>           
-    )
+  const styles = css;
+  return (
+    <Layout
+      pageTitle="Projects & developments"
+      pageSubTitle="Showcase of my latest works, projects and developments."
+      className={styles.root}
+    >
+      <Grid
+        container
+        justify="space-between"
+        alignItems="center"
+        direction="row"
+      >
+        <Grid xs={12} align="center" container>
+          <Query query={JOBS_QUERY} id={null}>
+            {({ data: { jobs } }) => {
+              return (
+                <>
+                  {jobs.map((job) => {
+                    return (
+                      // eslint-disable-next-line react/jsx-key
+                      <Grid lg={4} md={6} xs={12} item>
+                        <JobCard key={job.id} job={job} />
+                      </Grid>
+                    );
+                  })}
+                </>
+              );
+            }}
+          </Query>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 }
- 
-export default Portfolio
+
+export default Portfolio;
