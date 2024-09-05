83ec9ea5399ec67119ec58986766a4df455fefb2
Maicon Ribeiro Esteves <maicon.burn@gmail.com>
2024-09-05 17:31:01 +0100

    Add og-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,31 @@ export const metadata: Metadata = {
   title: "Maicon Ribeiro Esteves - Portfolio",
   description:
     "Software Engineer with a focus on React and Next.js and a passion for growth hacking.",
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+  ),
+  openGraph: {
+    title: "Maicon Ribeiro Esteves - Portfolio",
+    description:
+      "Explore Maicon's portfolio showcasing his work as a software engineer with expertise in React, Next.js, and growth hacking strategies.",
+    url: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
+    type: "website",
+    images: [
+      {
+        url: "/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Maicon Ribeiro Esteves - Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Maicon Ribeiro Esteves - Portfolio",
+    description:
+      "Explore Maicon's portfolio showcasing his work as a software engineer with expertise in React, Next.js, and growth hacking strategies.",
+    images: ["/images/og-image.png"],
+  },
 };
 
 export default function RootLayout({
